54951f50e0718a20256b72a4e2cfaf84226f1cad
dvldbgd <kalyan3x90@gmail.com>
2025-08-16 18:34:59 +0530

    refactor: split core/codes.go into multiple focused files

---
+++ codes/concurrency.go
@@ -0,0 +1,55 @@
+package codes
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+// RunExtractCommentsConcurrently runs ExtractComments in parallel for multiple files.
+func RunExtractCommentsConcurrently(files []string, maxWorkers int, tags string, ignoreErrors bool) map[string][]Comment {
+	if maxWorkers <= 0 {
+		maxWorkers = runtime.NumCPU()
+	}
+	if maxWorkers > len(files) {
+		maxWorkers = len(files)
+	}
+
+	results := make(map[string][]Comment)
+	var resultsMutex sync.Mutex
+	var waitGroup sync.WaitGroup
+	fileChannel := make(chan string)
+
+	// Worker goroutine for extracting comments
+	worker := func() {
+		defer waitGroup.Done()
+		for file := range fileChannel {
+			extractedComments, err := ExtractComments(file, tags)
+			if err != nil && !ignoreErrors {
+				fmt.Printf("Error processing %s: %v\n", file, err)
+			}
+			if extractedComments != nil {
+				resultsMutex.Lock()
+				results[file] = extractedComments
+				resultsMutex.Unlock()
+			}
+		}
+	}
+
+	waitGroup.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		go worker()
+	}
+
+	// Feed files into the worker pool
+	go func() {
+		for _, filePath := range files {
+			fileChannel <- filePath
+		}
+		close(fileChannel)
+	}()
+
+	waitGroup.Wait()
+	return results
+}
+
