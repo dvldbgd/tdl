e9704b8ba53c9969fba2be11a56451e26b74971a
znwng <kalyan3x90@gmail.com>
2025-08-25 08:32:48 +0530

    refactor(core): reorder functions so that called functions appear below caller functions for readability

--- core/core.go
+++ core/core.go
@@ -11,6 +11,57 @@ import (
 	"sync"
 )
 
+// singleLineCommentMap defines the mapping between comment characters and supported file extensions.
+var singleLineCommentMap = map[string][]string{
+	"//": {
+		".go", ".java", ".c", ".cpp", ".h", ".hpp", ".cs", ".swift", ".kt", ".rs", ".scala",
+		".ts", ".js", ".jsx", ".tsx",
+	},
+
+	"#": {
+		".py", ".rb", ".sh", ".bash", ".zsh", ".yml", ".yaml", ".toml", ".pl", ".pm", ".mk",
+		"makefile", "dockerfile", ".ini",
+	},
+
+	";": {
+		".lisp", ".clj", ".scm", ".s", ".asm",
+	},
+
+	"--": {
+		".lua", ".hs", ".sql", ".adb",
+	},
+
+	"'": {
+		".vb", ".vbs",
+	},
+
+	".. ": {".rst"},
+}
+
+// SupportedTags defines the set of recognized comment tags.
+var SupportedTags = map[string]struct{}{
+	"TODO":      {},
+	"FIXME":     {},
+	"NOTE":      {},
+	"HACK":      {},
+	"BUG":       {},
+	"OPTIMIZE":  {},
+	"DEPRECATE": {},
+}
+
+// extensionToCommentChar maps a file extension to its comment character for quick lookup.
+var extensionToCommentChar map[string]string
+
+// init initializes the extensionToCommentChar mapping.
+func init() {
+	extensionToCommentChar = make(map[string]string)
+	for commentChar, extensions := range singleLineCommentMap {
+		for _, extension := range extensions {
+			extensionToCommentChar[strings.ToLower(extension)] = commentChar
+		}
+	}
+}
+
 // RunExtractCommentsConcurrently runs ExtractComments in parallel for multiple files.
 func RunExtractCommentsConcurrently(
 	files []string,
@@ -61,57 +112,6 @@ func RunExtractCommentsConcurrently(
 
 	waitGroup.Wait()
 	return results
-}
-
-// singleLineCommentMap defines the mapping between comment characters and supported file extensions.
-var singleLineCommentMap = map[string][]string{
-	"//": {
-		".go", ".java", ".c", ".cpp", ".h", ".hpp", ".cs", ".swift", ".kt", ".rs", ".scala",
-		".ts", ".js", ".jsx", ".tsx",
-	},
-
-	"#": {
-		".py", ".rb", ".sh", ".bash", ".zsh", ".yml", ".yaml", ".toml", ".pl", ".pm", ".mk",
-		"makefile", "dockerfile", ".ini",
-	},
-
-	";": {
-		".lisp", ".clj", ".scm", ".s", ".asm",
-	},
-
-	"--": {
-		".lua", ".hs", ".sql", ".adb",
-	},
-
-	"'": {
-		".vb", ".vbs",
-	},
-
-	".. ": {".rst"},
-}
-
-// SupportedTags defines the set of recognized comment tags.
-var SupportedTags = map[string]struct{}{
-	"TODO":      {},
-	"FIXME":     {},
-	"NOTE":      {},
-	"HACK":      {},
-	"BUG":       {},
-	"OPTIMIZE":  {},
-	"DEPRECATE": {},
-}
-
-// extensionToCommentChar maps a file extension to its comment character for quick lookup.
-var extensionToCommentChar map[string]string
-
-// init initializes the extensionToCommentChar mapping.
-func init() {
-	extensionToCommentChar = make(map[string]string)
-	for commentChar, extensions := range singleLineCommentMap {
-		for _, extension := range extensions {
-			extensionToCommentChar[strings.ToLower(extension)] = commentChar
-		}
-	}
 }
 
 // ExtractComments reads a file line by line and extracts comments containing specified tags.
